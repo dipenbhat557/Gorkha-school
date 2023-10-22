40644394727aad68255a6ff77d925cc6b5cb941d
dipenbhat557 <dipenbhat557@gmail.com>
2023-10-22 23:14:32 +0530

    :lipstick: fixed responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <nav className="sm:px-16 px-6 w-full flex items-center py-4 bg-blue-400 z-20 relative">
-      <div className="w-full flex items-center justify-between">
+      <div className="w-full flex items-center justify-evenly sm:justify-between">
         <ul className="list-none sm:flex gap-6 md:gap-10 hidden">
           {navLinks.map((link) => (
             <li
@@ -22,43 +22,42 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <div className="flex flex-row justify-center">
+        <div className="flex flex-row justify-center items-center">
           <input
-            className="rounded-xl p-1 pl-4 ml-6 md:ml-0 max-w-[70%] sm:max-w-[50%]"
+            className="rounded-xl p-1 pl-4 ml-6 md:ml-0 max-w-[70%] sm:max-w-[65%]"
             type="text"
             placeholder="Type to search"
           />
           <AiOutlineSearch className="m-2 text-2xl text-white hover:bg-blue-500 hover:text-4xl hover:rounded-xl hover:text-white hover:p-2" />
-
-          {/* Mobile menu */}
-          <div className="sm:hidden flex items-center">
-            <img
-              src={toggle ? "src/assets/close.png" : "src/assets/menu.png"}
-              alt="menu"
-              className="w-[30px] h-[30px] object-contain cursor-pointer rounded-3xl text-2xl "
-              onClick={() => setToggle(!toggle)}
-            />
-            <ul
-              className={`${
-                !toggle ? "hidden" : "flex"
-              } p-6 bg-blue-400 absolute top-12 right-0 mx-4 my-4 min-w-[140px] z-10 rounded-xl flex flex-col items-end gap-4`}
-            >
-              {navLinks.map((link) => (
-                <li
-                  key={link.id}
-                  className={`${
-                    active === link.title ? "text-white" : "text-secondary"
-                  } font-poppins text-[16px] font-medium cursor-pointer`}
-                  onClick={() => {
-                    setToggle(!toggle);
-                    setActive(link.title);
-                  }}
-                >
-                  <a href={`#${link.id}`}>{link.title}</a>
-                </li>
-              ))}
-            </ul>
-          </div>
+        </div>
+        {/* Mobile menu */}
+        <div className="sm:hidden flex items-center">
+          <img
+            src={toggle ? "src/assets/close.png" : "src/assets/menu.png"}
+            alt="menu"
+            className="w-[30px] h-[30px] object-contain cursor-pointer rounded-3xl text-2xl"
+            onClick={() => setToggle(!toggle)}
+          />
+          <ul
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 bg-blue-400 absolute top-12 right-0 mx-4 my-4 min-w-[140px] z-10 rounded-xl flex flex-col items-end gap-4`}
+          >
+            {navLinks.map((link) => (
+              <li
+                key={link.id}
+                className={`${
+                  active === link.title ? "text-white" : "text-secondary"
+                } font-poppins text-[16px] font-medium cursor-pointer`}
+                onClick={() => {
+                  setToggle(!toggle);
+                  setActive(link.title);
+                }}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </nav>
